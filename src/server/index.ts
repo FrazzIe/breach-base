743463d216399c9735136ebd99ef5bd7a6c471f1
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-14 10:50:36 +0100

    empty index files

--- src/server/index.ts
+++
@@ -1,4 +0,0 @@
-import { Cake } from "../shared/index";
-setImmediate(() => {
-	console.log(`I LOVE ${Cake()}`);
-});
--- src/shared/index.ts
+++
@@ -1,7 +0,0 @@
-export function Cake(): string {
-	/// #if SERVER
-	return "Carrot Cake";
-	/// #elif CLIENT
-	return "Chocolate Cake"
-	/// #endif
-}
