1d3e89b6b310be95be3f0df130f077d1212d6f6a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-15 02:52:06 +0100

    add insert func

--- src/server/module/mongodb/crud/create.ts
+++ src/server/module/mongodb/crud/create.ts
@@ -1,4 +1,14 @@
-import { Db } from "mongodb";
+import { Collection, Db, InsertOneWriteOpResult, InsertWriteOpResult, ObjectId } from "mongodb";
+import { Wait } from "../../../../shared/utils";
+
+interface IParams {
+	collection: string;
+	document: {} | Array<{}>;
+}
+
+interface ICallback {
+	(success: number, count: number, insertedId: ObjectId | { [key: number]: any; }): void;
+}
 
 class DbCreate {
 	private initialised: boolean;
@@ -23,7 +33,48 @@ class DbCreate {
 		this.initialised = _initialised;
 	}
 
+	/**
+	 * Insert one or more documents into a collection
+	 * @param {IParams} params The insert object
+	 * @param {string} params.collection The MongoDB collection name
+	 * @param {{} | Array<{}>} params.document MongoDB documents to be inserted
+	 * @param {ICallback} callback The callback object
+	 * @returns {Promise<void>} Empty promise
+	 */
+	public async Insert(params: IParams, callback?: ICallback): Promise<void> {
+		if (!this.initialised) {
+			while (!this.initialised) {
+				await Wait(10);
+			}
+		}
 
+		if (!params.collection) {
+			console.warn("MongoDB couldn't find collection in params");
+			return;
+		}
+
+		if (!params.document) {
+			console.warn("MongoDB couldn't find document(s) in params");
+			return;
+		}
+
+		const collection: Collection = this.database.collection(params.collection);
+		const insertMany: boolean = Array.isArray(params.document);
+
+		if (insertMany) {
+			const insert: InsertWriteOpResult<any> = await collection.insertMany(params.document as Array<{}>);
+
+			if (callback)
+				callback(insert.result.ok, insert.insertedCount, insert.insertedIds);
+			return;
+		}
+
+		const insert: InsertOneWriteOpResult<any> = await collection.insertOne(params.document);
+
+		if (callback)
+			callback(insert.result.ok, insert.insertedCount, insert.insertedId);
+		return;
+	}
 }
 
 export default new DbCreate();
