c283a0e1957a29d1a93273c43eef60fe2e28bb5d
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-15 15:51:05 +0100

    add update class to mongodb idx

--- src/server/module/mongodb/index.ts
+++ src/server/module/mongodb/index.ts
@@ -1,12 +1,14 @@
 import { Db } from "mongodb";
 import Connector from "./connector";
 import DbCreate from "./crud/create";
+import DbUpdate from "./crud/update";
 
 let database: Db = null;
 
 function OnConnectSuccess(result: boolean) {
 	database = Connector.Database;
 	DbCreate.Init(database, result);
+	DbUpdate.Init(database, result);
 	console.log("OK");
 }
 
