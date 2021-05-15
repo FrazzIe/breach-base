186d4c8c581389f557d89a494d78c02482dde4ec
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-15 13:43:35 +0100

    create DbUpdate class

---
+++ src/server/module/mongodb/crud/update.ts
@@ -0,0 +1,20 @@
+import DbMain from "./main";
+
+class DbUpdate extends DbMain {
+	/**
+	 * Initialise class members
+	 * @constructor
+	 */
+	constructor() {
+		super();
+	}
+
+	/**
+	 *
+	 */
+	public async Update(): Promise<void> {
+		return;
+	}
+}
+
+export default new DbUpdate();
