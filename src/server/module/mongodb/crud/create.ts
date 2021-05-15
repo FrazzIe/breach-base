09da9374236d873f9c183b62baf4bc5bf6ef97b1
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-15 02:24:37 +0100

    add comments

--- src/server/module/mongodb/crud/create.ts
+++ src/server/module/mongodb/crud/create.ts
@@ -4,11 +4,20 @@ class DbCreate {
 	private initialised: boolean;
 	private database: Db;
 
+	/**
+	 * Initialise class members
+	 * @constructor
+	 */
 	constructor() {
 		this.initialised = false;
 		this.database = null;
 	}
 
+	/**
+	 * Sets the database and connection status
+	 * @param _database Database instance
+	 * @param _initialised Connection result
+	 */
 	public Init(_database: Db, _initialised: boolean): void {
 		this.database = _database;
 		this.initialised = _initialised;
