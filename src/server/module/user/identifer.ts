5879b748290401f7561ab0b6c238a75b4da6f28a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-19 16:59:31 +0100

    change var names, again..

--- src/server/module/user/identifer.ts
+++ src/server/module/user/identifer.ts
@@ -1,13 +1,13 @@
 import { IIdentifierList } from "../../../shared/utils/identifier";
 
 //Identifiers that are required
-export const requiredIds: string[] = ["license2"];
-const requiredIdsMessage: string[] = [
+const requiredIdentifiers: string[] = ["license2"];
+const requiredIdentifierMessages: string[] = [
 	"Unable to retrieve your R* Id, is your R* account linked?",
 ];
 //At least one of the identifers are required
-export const oneOfId: string[] = ["discord", "steam"];
-const oneOfIdMessage: string = "You must link your discord or steam account to join this server!";
+const requiredIdentifier: string[] = ["discord", "steam"];
+const requiredIdentifierMessage: string = "You must link your discord or steam account to join this server!";
 
 /**
  * Check if a player has the required identifers needed to play
@@ -15,17 +15,20 @@ const oneOfIdMessage: string = "You must link your discord or steam account to j
  * @returns {[boolean, string]} Returns if the collection contains the required identifiers and an optional error message
  */
 export function HasRequiredIdentifiers(ids: IIdentifierList): [result: boolean, message?: string] {
-	for (let idx = 0; idx < requiredIds.length; idx++) {
-		const id = requiredIds[idx];
+	for (let idx = 0; idx < requiredIdentifiers.length; idx++) {
+		const id = requiredIdentifiers[idx];
 		if (!ids[id])
-			return [false, requiredIdsMessage[idx]];
+			return [false, requiredIdentifierMessages[idx]];
 	}
 
-	for (let idx = 0; idx < oneOfId.length; idx++) {
-		const id = oneOfId[idx];
+	for (let idx = 0; idx < requiredIdentifier.length; idx++) {
+		const id = requiredIdentifier[idx];
 		if (ids[id])
 			return [true];
 	}
 
-	return [false, oneOfIdMessage];
+	return [false, requiredIdentifierMessage];
 }
+
+export const RequiredIdentifiers: string[] = requiredIdentifiers;
+export const RequiredIdentifier: string[] = requiredIdentifier;
